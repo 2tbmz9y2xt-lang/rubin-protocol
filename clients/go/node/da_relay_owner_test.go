a59e44d826408d0650a7105342b3db16c5ce6acd
2tbmz9y2xt-lang <245613818+2tbmz9y2xt-lang@users.noreply.github.com>
2026-09-20 12:07:23 +0200

    RUB-1406: add dormant DA complete-set capacity planner

--- clients/go/node/da_relay_owner_test.go
+++ clients/go/node/da_relay_owner_test.go
@@ -165,7 +165,7 @@ func requireDAAdmissionStructure(t *testing.T) {
 		for _, spec := range []struct {
 			name, dir         string
 			goFiles, cgoFiles int
-		}{{"node", ".", 64, 0}, {"consensus", "../consensus", 60, 3}} {
+		}{{"node", ".", 65, 0}, {"consensus", "../consensus", 60, 3}} {
 			pkg, err := context.ImportDir(spec.dir, 0)
 			if err != nil {
 				t.Fatal(err)
